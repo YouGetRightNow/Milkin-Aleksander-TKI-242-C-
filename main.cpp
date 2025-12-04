17d925b06a57b8e53ff7d93380d9f351c70a01f1
YouGetRightNow <milkin.aleks2017@yandex.ru>
2025-12-04 11:56:27 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,70 +7,73 @@
 #include <iostream>
 #include <string>
 #include <locale.h>
+#include <functional>
+#include <limits>
 
 using namespace std;
 using namespace algebra;
 
-enum class FillMethod {
-	RANDOM = 1,
-	MANUAL = 2
-};
-
 int main()
 {
-	setlocale(LC_ALL, "RU");
+    setlocale(LC_ALL, "RU");
 
-	size_t size;
-	cout << "Введите размер массива: ";
-	cin >> size;
+    size_t size;
+    cout << "Введите размер массива: ";
+    cin >> size;
 
-	Matrix<int> matrix(size);
+    Matrix<int> matrix(size);
 
-	cout << "Выберите способ заполнения массива:\n";
-	cout << "1 - Случайными числами\n";
-	cout << "2 - Вводом с клавиатуры\n";
+	enum FillMethod { MANUAL, RANDOM };
 
-	int choice;
-	cin >> choice;
+    cout << "Выберите способ заполнения массива ("
+              << MANUAL << " - ручной ввод, "
+              << RANDOM << " - случайные числа): ";
 
-	Generator* generator = nullptr;
+    int choice;
+    cin >> choice;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-	switch (static_cast<FillMethod>(choice)) {
-	case FillMethod::RANDOM:
-		generator = new RandomGenerator();
-		cout << "Массив будет заполнен случайными числами от -1000 до 1000" << endl;
-		break;
+    Generator* generator = nullptr;
 
-	case FillMethod::MANUAL:
-		generator = new IStreamGenerator(cin);
-		cout << "Введите элементы массива:" << endl;
-		break;
+    switch ((FillMethod)choice)
+    {
+        case MANUAL:
+        {
+            generator = new IStreamGenerator(cin);
+            cout << "Введите элементы массива:" << endl;
+        }
+        break;
 
-	default:
-		cout << "Неверный выбор. Будет использован случайный генератор." << endl;
-		generator = new RandomGenerator();
-		break;
-	}
+        case RANDOM:
+        {
+            generator = new RandomGenerator();
+            cout << "Массив будет заполнен случайными числами от -1000 до 1000" << endl;
+        }
+        break;
 
-	matrix.fill(*generator);
-	cout << "Исходный массив: " << matrix << endl;
-
-
-	ConcreteExercise1<int> exercise1(matrix, *generator);
-	exercise1.task();
-	cout << "Массив после выполнения 1 условия: " << exercise1.getMatrix() << endl;
-
-
-	ConcreteExercise2<int> exercise2(matrix, *generator);
-	exercise2.task();
-	cout << "Массив после выполнения 2 условия: " << exercise2.getMatrix() << endl;
+        default:
+            cout << "Неверный выбор. Будет использован случайный генератор." << endl;
+            generator = new RandomGenerator();
+            break;
+    }
 
 
-	ConcreteExercise3<int> exercise3(matrix, *generator);
-	exercise3.task();
-	cout << "Массив после выполнения 3 условия: " << exercise3.getMatrix() << endl;
+    matrix.fill(*generator);
+    cout << "Исходный массив: " << matrix << endl;
 
-	delete generator;
+    ConcreteExercise1<int> exercise1(matrix, *generator);
+    exercise1.task();
+    cout << "Массив после выполнения 1 условия: " << exercise1.getMatrix() << endl;
 
-	return 0;
+    ConcreteExercise2<int> exercise2(matrix, *generator);
+    exercise2.task();
+    cout << "Массив после выполнения 2 условия: " << exercise2.getMatrix() << endl;
+
+    ConcreteExercise3<int> exercise3(matrix, *generator);
+    exercise3.task();
+    cout << "Массив после выполнения 3 условия: " << exercise3.getMatrix() << endl;
+
+    delete generator;
+
+    return 0;
 }
