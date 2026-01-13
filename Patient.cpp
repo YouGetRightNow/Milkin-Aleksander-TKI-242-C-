d5174a0ac603eda64f9571983dfa6c45f04e45d1
YouGetRightNow <milkin.aleks2017@yandex.ru>
2026-01-13 15:15:28 +0300

    Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -1,12 +1,14 @@
 #include "Patient.h"
 #include <sstream>
+#include <algorithm>
 
 Patient::Patient(const std::string& firstName, const std::string& lastName,
-    const std::string& middleName, int age,
-    const std::string& medicalCardNumber,
-    const std::string& phoneNumber, const std::string& address)
-    : Person(firstName, lastName, middleName, age),
-    medicalCardNumber(medicalCardNumber), phoneNumber(phoneNumber), address(address) 
+                 const std::string& middleName, int age,
+                 const std::string& documentId,
+                 const std::string& medicalCardNumber,
+                 const std::string& phoneNumber, const std::string& address)
+    : Person(firstName, lastName, middleName, age, documentId),
+      medicalCardNumber(medicalCardNumber), phoneNumber(phoneNumber), address(address) 
 {
 }
 
@@ -14,9 +16,48 @@ std::string Patient::getInfo() const
 {
     std::stringstream ss;
     ss << "Пациент: " << getFullName() << "\n"
-        << "Возраст: " << age << " лет\n"
-        << "Номер мед. карты: " << medicalCardNumber << "\n"
-        << "Телефон: " << phoneNumber << "\n"
-        << "Адрес: " << address << "\n";
+       << "Документ: " << documentId << "\n"
+       << "Возраст: " << age << " лет\n"
+       << "Номер мед. карты: " << medicalCardNumber << "\n"
+       << "Телефон: " << phoneNumber << "\n"
+       << "Адрес: " << address << "\n"
+       << "Количество записей: " << appointments.size() << "\n";
     return ss.str();
+}
+
+void Patient::addAppointment(std::shared_ptr<Appointment> appointment)
+{
+    appointments.push_back(appointment);
+}
+
+bool Patient::removeAppointment(std::shared_ptr<Appointment> appointment)
+{
+    auto it = std::find(appointments.begin(), appointments.end(), appointment);
+    if (it != appointments.end()) {
+        appointments.erase(it);
+        return true;
+    }
+    return false;
+}
+
+std::vector<std::shared_ptr<Appointment>> Patient::getUpcomingAppointments() const
+{
+    std::vector<std::shared_ptr<Appointment>> upcoming;
+    std::copy_if(appointments.begin(), appointments.end(),
+                 std::back_inserter(upcoming),
+                 [](const std::shared_ptr<Appointment>& app) {
+                     return !app->getIsCompleted();
+                 });
+    return upcoming;
+}
+
+std::vector<std::shared_ptr<Appointment>> Patient::getCompletedAppointments() const
+{
+    std::vector<std::shared_ptr<Appointment>> completed;
+    std::copy_if(appointments.begin(), appointments.end(),
+                 std::back_inserter(completed),
+                 [](const std::shared_ptr<Appointment>& app) {
+                     return app->getIsCompleted();
+                 });
+    return completed;
 }
