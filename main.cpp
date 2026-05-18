c78ff97d9335285aca2bf63f7d5a70cf28503f12
YouGetRightNow <milkin.aleks2017@yandex.ru>
2026-05-18 17:55:22 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -9,28 +9,42 @@ using namespace std;
 int main() 
 {
     setlocale(LC_ALL, "RU");
-    vector<int> V = { 5, 2, 8, 2, 5, 6, 8, 1, 9, 2, 5 };
-    cout << "Исходный вектор V: ";
+    
+    vector<int> V;
+    int n, element;
+    
+    cout << "Введите количество элементов вектора: ";
+    cin >> n;
+    
+    cout << "Введите " << n << " целых чисел:" << endl;
+    for (int i = 0; i < n; i++) 
+    {
+        cout << "Элемент " << i + 1 << ": ";
+        cin >> element;
+        V.push_back(element);
+    }
+    
+    cout << "\nИсходный вектор V: ";
     for_each(V.begin(), V.end(), [](int value) 
-        {
+    {
         cout << value << " ";
-        });
+    });
     cout << endl << endl;
 
     map<int, int> M;
 
     for_each(V.begin(), V.end(), [&M](int value) 
-        {
+    {
         M[value]++;
-        });
+    });
 
     cout << "Элемент | Количество повторений" << endl;
     cout << "------------------------" << endl;
 
     for_each(M.begin(), M.end(), [](const auto& pair) 
-        {
+    {
         cout << "   " << pair.first << "    " << pair.second << endl;
-        });
+    });
 
     return 0;
 }
