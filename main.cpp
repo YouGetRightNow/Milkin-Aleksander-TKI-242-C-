96d282fb10ba1665c047353ab2e512fd75093088
YouGetRightNow <milkin.aleks2017@yandex.ru>
2026-05-26 20:59:25 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -8,14 +8,29 @@
 
 using namespace std;
 
+/**
+ * @brief Функциональный объект для сравнения целых чисел по их абсолютным значениям.
+ * @details Возвращает true, если |a| < |b|.
+ */
 struct less_abs : public binary_function<int, int, bool>
 {
+    /**
+     * @brief Оператор сравнения по модулю.
+     * @param a Первое число.
+     * @param b Второе число.
+     * @return true, если |a| < |b|, иначе false.
+     */
     bool operator()(const int a, const int b) const
     {
         return abs(a) < abs(b);
     }
 };
 
+/**
+ * @brief Поиск первой соседней пары чисел, нарушающей строгое возрастание модулей.
+ * @details Выполняет ввод данных, поиск целевой пары и вывод результата.
+ * @return int 0 при успешном завершении.
+ */
 int main()
 {
     setlocale(LC_ALL, "RU");
@@ -24,8 +39,8 @@ int main()
     cout << "Введите целые числа: ";
 
     copy(istream_iterator<int>(cin),
-        istream_iterator<int>(),
-        back_inserter(V));
+         istream_iterator<int>(),
+         back_inserter(V));
 
     auto it = adjacent_find(V.begin(), V.end(), not2(less_abs()));
 
